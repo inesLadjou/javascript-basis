0ee2d1d97da4577f53e003b7125aa88e435eda95
inesLadjou <lanademanou@gmail.com>
2024-08-20 10:45:32 +0200

    up to date

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -1,6 +1,6 @@
 let count = 0; // Initialize count to 0
 function increaseCount() {
-  count++; // Increment the count by 1
+  count++ // Increment the count by 1
   displayCount(); // Display the count
   checkCountValue(); // Check count value and display messages
 }
@@ -16,7 +16,13 @@ function checkCountValue() {
 //function to display count in the HTML element with the ID 
 //countDisplay and updates its content 
 //to reflect the current count value.
-
 function displayCount() {
-document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+document.getElementById('countDisplay').innerHTML= count; // Display the count in the HTML
+}
+
+//function to reset the count to 0
+function countReset(){ 
+  count = 0; // reset count to 0
+  displayCount (); // show the reseted count in the html
+  alert ("your Followers count has been reset");
 }
