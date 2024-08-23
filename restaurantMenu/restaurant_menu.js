cf709e693a91cd91dea01381faefd40d0835c526
inesLadjou <lanademanou@gmail.com>
2024-08-23 11:43:01 -0400

    arrays,for loop,foreach method and map method in js

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,23 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+//for loop, map method and forEach method to 
+//traverse through these arrays and 
+//display the menu items on the HTML page
+
+// 1.iterate through the  
+//breakfastMenu array using map method.
+ const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+        document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+//Defining forEach array method and for loop iteration
+ let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
